258fe97fe2cabc4ce22898afa08bb3dcb8d38518
Aashi <aashiaggarwal28552@gmail.com>
2025-05-19 23:14:07 +0530

    show generated interviews

--- mock_interview/app/(root)/page.tsx
+++ mock_interview/app/(root)/page.tsx
@@ -1,16 +1,22 @@
 import InterviewCard from '@/components/InterviewCard'
 import { Button } from '@/components/ui/button'
 import { dummyInterviews } from '@/constants'
-import { getCurrentUser, getInterviewByUserId } from '@/lib/actions/auth.actions'
+import { getCurrentUser, getInterviewByUserId, getLatestInterviews } from '@/lib/actions/auth.actions'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const Page = async() => {
   const user = await getCurrentUser();
-  const userInterviews = await getInterviewByUserId(user?.id!);
+
+  const [userInterviews, latestInterviews] = await Promise.all([
+    await getInterviewByUserId(user?.id!),
+    await getLatestInterviews({userId: user?.id!})
+
+  ])
 
   const hasPastInterviews = userInterviews?.length > 0
+  const hasUpcomingInterviews = latestInterviews?.length > 0
   return (
     <>
       <section className='card-cta'>
@@ -32,7 +38,7 @@ const Page = async() => {
         <h2>Your Interviews</h2>
         <div className='interviews-section'>
           {
-            hasPastInterviews ? ( userInterviews?.map((interview) => (
+            hasUpcomingInterviews ? ( userInterviews?.map((interview) => (
               <InterviewCard {...interview} key={interview.id}/>
               
             ))) : (<p> You haven&apos;t started any interviews yet</p>
@@ -46,10 +52,13 @@ const Page = async() => {
       <section className='flex flex-col gap-6 mt-8'>
         <h2>Take an Interview</h2>
         <div className='interviews-section'>
-        {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
-          <p> There are no interviews available</p>
+        {
+            hasPastInterviews ? ( latestInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id}/>
+              
+            ))) : (<p> There are no  new interviews available</p>
+            )
+          }
 
         </div>
 
